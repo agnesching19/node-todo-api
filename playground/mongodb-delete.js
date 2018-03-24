15797d5dd863552b0cc18518286cad35dc9941c9
Agnes Ching <agnesching19@gmail.com>
2018-03-24 10:37:49 +0000

    mongodb-delete refactor

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,25 +8,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   console.log('Connected to MongoDB server');
 
   // deleteMany
-  // db.collection('Todos').deleteMany({text: 'Writing diary'}).then((result) => {
-  //   console.log(result);
-  // });
-  // db.collection('Users').deleteMany({name: 'Agnes'}).then((result) => {
-  //   console.log(result);
-  // });
+  db.collection('Todos').deleteMany({text: 'Writing diary'}).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').deleteMany({name: 'Agnes'}).then((result) => {
+    console.log(result);
+  });
 
   // deleteOne
-  // db.collection('Todos').deleteOne({text: 'Fanpage updates'}).then((result) => {
-  //   console.log(result);
-  // });
-  // db.collection('Users').deleteOne({age: 35}).then((result) => {
-  //   console.log(result);
-  // });
+  db.collection('Todos').deleteOne({text: 'Fanpage updates'}).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').deleteOne({age: 35}).then((result) => {
+    console.log(result);
+  });
 
   // findOneAndDelete
-  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
-  //   console.log(result);
-  // });
+  db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    console.log(result);
+  });
+
   db.collection('Users').findOneAndDelete({_id: 123}).then((result) => {
     console.log(result);
   });
