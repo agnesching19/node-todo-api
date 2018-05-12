deb8ad75877ce1d56b466ac2830dd421238c4c78
Agnes Ching <agnesching19@gmail.com>
2018-05-12 15:36:45 +0100

    POST /users/login route

--- server/server.js
+++ server/server.js
@@ -114,7 +114,6 @@ app.post('/users', (req, res) => {
   });
 });
 
-
 // GET /users/me
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
@@ -122,6 +121,19 @@ app.get('/users/me', authenticate, (req, res) => {
 
 app.listen(port, () => {
   console.log(`Started up on port ${port}`);
+});
+
+//POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 module.exports = {app};
