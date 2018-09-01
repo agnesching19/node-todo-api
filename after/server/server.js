5b186154db4e6112e08f8d8f9030303122111ebe
Agnes Ching <agnesching19@gmail.com>
2018-09-01 16:04:00 +0100

    Refactor with Async/Await

---
+++ after/server/server.js
@@ -0,0 +1,144 @@
+require('./config/config');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+// POST /todos
+app.post('/todos', authenticate, async (req, res) => {
+  try {
+    const todo = new Todo({
+      text: req.body.text,
+      _creator: req.user._id
+    });
+  
+    const doc = await todo.save();
+    res.send(doc);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// GET /todos
+app.get('/todos', authenticate, async (req, res) => {
+  try {
+    const todos = await Todo.find({
+      _creator: req.user._id
+    });
+    res.send({todos});
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// GET /todos/:id
+app.get('/todos/:id', authenticate, async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) return res.status(404).send();
+
+    const todo = await Todo.findOne({
+      _id: id,
+      _creator: req.user._id
+    });
+
+    if (!todo) return res.status(404).send();
+    res.send({todo});
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+// DELETE /todos/:id
+app.delete('/todos/:id', authenticate, async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) return res.status(404).send();
+
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id
+    });
+
+    if (!todo) return res.status(404).send();
+    res.send({todo});
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+// PATCH /todos/:id
+app.patch('/todos/:id', authenticate, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) return res.status(404).send();
+
+    if (_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    const todo = await Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true});
+    if (!todo) return res.status(404).send();
+    res.send({todo});
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+// POST /users
+app.post('/users', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// GET /users/me
+app.get('/users/me', authenticate, (req, res) => res.send(req.user));
+
+app.listen(port, () => console.log(`Started up on port ${port}`));
+
+//POST /users/login {email, password}
+app.post('/users/login', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//DELETE /users/me/token
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token);
+    res.status(200).send();
+  } catch (e) {
+    res.status(400).send(); 
+  }
+});
+
+module.exports = {app};
---
+++ before/playground/mongodb-delete.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // deleteMany
+  db.collection('Todos').deleteMany({text: 'Writing diary'}).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').deleteMany({name: 'Agnes'}).then((result) => {
+    console.log(result);
+  });
+
+  // deleteOne
+  db.collection('Todos').deleteOne({text: 'Fanpage updates'}).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').deleteOne({age: 35}).then((result) => {
+    console.log(result);
+  });
+
+  // findOneAndDelete
+  db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('Users').findOneAndDelete({_id: 123}).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
